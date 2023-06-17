da71d82eea4163e16639d73c8c5d62f023dbe545
Chris Szentkiralyi <chris.szentkiralyi@gmail.com>
2023-06-16 22:17:49 -0600

    Indentation cleanup

--- src/lib/hydra.js
+++ src/lib/hydra.js
@@ -1,14 +1,14 @@
 /* TODO: figure out wtf the "hydre:XXX" strings mean */
 class HydraContent {
-    constructor(jsonStr) {
-        this.data = JSON.parse(jsonStr);
-        this.context = this.data['@context'];
-        this.id = this.data['@id'];
-        this.item = {
-            type: this.data['@type'],
-            value: this.data['hydra:member']
-        };
-    }
+  constructor(jsonStr) {
+    this.data = JSON.parse(jsonStr);
+    this.context = this.data['@context'];
+    this.id = this.data['@id'];
+    this.item = {
+      type: this.data['@type'],
+      value: this.data['hydra:member']
+    };
+  }
 }
 
 module.exports = HydraContent;
